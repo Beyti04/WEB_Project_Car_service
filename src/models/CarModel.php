3d36fe603ca2cf2f1bf406a2339486881d72dff1
Emre Idaet <98050381+EmreIdaet@users.noreply.github.com>
2025-11-20 12:41:35 +0200

    add CarModel

---
+++ src/models/CarModel.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Models;
+
+use Config\Database;
+use PDO, PDOException;
+
+class CarModel{
+    private PDO $db;
+
+    public function __construct(
+        public ?int $id = null,
+        public int $brand_id,
+        public string $model_name
+    ) {
+        $this->db = Database::getInstance();
+    }
+
+    public function save(): bool {
+        $sql = "INSERT INTO car_model (brand_id, model_name) 
+                VALUES (?, ?)";
+        
+        try {
+            $stmt = $this->db->prepare($sql);
+            $success = $stmt->execute([
+                $this->brand_id,
+                $this->model_name
+            ]);
+
+            if ($success) {
+                $this->id = (int)$this->db->lastInsertId();
+            }
+            return $success;
+        } catch (PDOException $e) {
+            error_log("CarModel Save Error: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    public static function getAllModels(): array {
+        $db = Database::getInstance();
+        $sql = "SELECT * FROM car_model";
+
+        try {
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+            $models = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $models;
+        } catch (PDOException $e) {
+            error_log("CarModel Fetch Error: " . $e->getMessage());
+            return [];
+        }
+    }
+
+    public static function getModelsByBrand(int $brand_id): array {
+        $db = Database::getInstance();
+        $sql = "SELECT * FROM car_model WHERE brand_id = ?";
+
+        try {
+            $stmt = $db->prepare($sql);
+            $stmt->execute([$brand_id]);
+            $models = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $models;
+        } catch (PDOException $e) {
+            error_log("CarModel Fetch By Brand Error: " . $e->getMessage());
+            return [];
+        }
+    }
+}
